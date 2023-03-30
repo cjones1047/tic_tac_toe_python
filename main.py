ee944be5b43872c85f59c3fd406ca02781e83886
cjones1047 <cjmj1047@gmail.com>
2023-03-30 07:07:46 -0500

    list of lists of winning codes created and current_player is switched at end of each iteration of while loop

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 board_ascii = """
 
     1     2     3  
@@ -10,7 +12,6 @@ B      |     |
        |     |     
 C      |     |     
        |     | 
-       
 """
 
 board_list = list(board_ascii)
@@ -27,13 +28,31 @@ board_cell_dict = {
     'C3': 178
 }
 
+winning_codes = [
+    ['A1', 'A2', 'A3'],
+    ['B1', 'B2', 'B3'],
+    ['C1', 'C2', 'C3'],
+    ['A1', 'B1', 'C1'],
+    ['A2', 'B2', 'C2'],
+    ['A3', 'B3', 'C3'],
+    ['A1', 'B2', 'C3'],
+    ['A3', 'B2', 'C1']
+]
+
+players = {
+    'X': {},
+    'O': {}
+}
+
 play_game = True
+current_player = random.choice(['X', 'O'])
 while play_game:
     print(''.join(board_list))
-    player_move = input('Player "X" move by entering box number (such as "A1" for top-left corner):\n').upper()
+    player_move = input(f'Player {current_player} move by entering box number (such as "A1" for top-left corner):'
+                        f'\n').upper()
     if player_move in board_cell_dict:
         print(board_cell_dict[player_move])
     else:
         print('Invalid move.')
         continue
-    print('Valid move.')
+    current_player = [player for player in players if player != current_player][0]
